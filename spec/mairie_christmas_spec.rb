5e1f8a082111446d65c59be15b5171bd72c4ef43
WinnyDrancourt <winny.morel@orange.fr>
2024-01-25 17:42:28 +0100

    try rspec

--- spec/mairie_christmas_spec.rb
+++ spec/mairie_christmas_spec.rb
@@ -2,6 +2,6 @@ require_relative '../lib/mairie_christmas'
 
 describe "Townhall email method" do
   it "Should take one mail" do
-    expect(find_mail(https://annuaire-des-mairies.com/95/saint-brice-sous-foret.html)).to eq("info@saintbrice95.fr")
+    expect(make_hash()).to be_kind_of(Hash)
   end
 end
